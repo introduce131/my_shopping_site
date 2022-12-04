2e39d67e4631dbb8c7a4d2ff1fdce21e3107ce8c
jongwon Baek <introduce131@naver.com>
2022-12-05 00:45:20 +0900

    2022-12-05 modified

--- src/components/MainSlideImage.jsx
+++ src/components/MainSlideImage.jsx
@@ -1,5 +1,14 @@
 function MainSlideImage() {
   const MainImageCnt = 7;
+
+  for (let i = 0; i < MainImageCnt; i++) {
+    var newInputRadio = document.createElement("input");
+    newInputRadio.setAttribute("type", "radio");
+    newInputRadio.setAttribute("name", "slide");
+    newInputRadio.setAttribute("id", `slide0${i}`);
+  }
+
+  <div className="section"></div>;
 }
 
 export default MainSlideImage;
