71a12c6ad0400735e25c3164ef7f00b42d2c968d
jongwon Baek <introduce131@naver.com>
2022-12-13 12:22:21 +0900

    2022-12-13 modified

--- src/components/MainSlideImage.jsx
+++ src/components/MainSlideImage.jsx
@@ -1,28 +1,13 @@
 import Slider from "react-slick";
+import styled from "styled-components";
 import "../css/slick.css";
 import "../css/slick-theme.css";
 
-const PrevArrow = (props) => {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "green" }}
-      onClick={onClick}
-    />
-  );
-};
-
-const NextArrow = (props) => {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "black" }}
-      onClick={onClick}
-    />
-  );
-};
+const Image = styled.img`
+  margin: auto;
+  width: 1103px;
+  height: 612px;
+`;
 
 const MainSlideImage = () => {
   const settings = {
@@ -36,22 +21,23 @@ const MainSlideImage = () => {
     autoplaySpeed: 10000, //자동 스크롤 시 다음 슬라이드 뜨는 시간(ms)
     vertical: false, //세로 방향 옵션 true:세로, false:가로
     arrows: true, //옆으로 이동하는 화살표 표시 여부
-    prevArrow: <PrevArrow />,
+    prevArrow: <img src={`${process.env.PUBLIC_URL}/images/left-arrow.png`} />,
+    nextArrow: <img src={`${process.env.PUBLIC_URL}/images/right-arrow.png`} />,
     dotsClass: "slick-dots", //아래 나오는 점(페이지네이션) css class 지정
     dragable: "true", //드래그 가능 여부
   };
 
   return (
-    <div className="carousel">
+    <div>
       <Slider {...settings}>
         <div>
-          <img src={process.env.PUBLIC_URL + "/images/image01.jpg"} />
+          <Image src={process.env.PUBLIC_URL + "/images/image01.png"} />
         </div>
         <div>
-          <img src={process.env.PUBLIC_URL + "/images/image02.jpg"} />
+          <Image src={process.env.PUBLIC_URL + "/images/image02.jpg"} />
         </div>
         <div>
-          <img src={process.env.PUBLIC_URL + "/images/image03.jpg"} />
+          <Image src={process.env.PUBLIC_URL + "/images/image03.jpg"} />
         </div>
       </Slider>
     </div>
