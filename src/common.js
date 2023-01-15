2178f6929478e176078c9779eb300e2b81000cb4
jongwon Baek <introduce131@naver.com>
2023-01-16 01:09:43 +0900

    not modified

--- src/common.js
+++ src/common.js
@@ -7,25 +7,39 @@ export const addCommas = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g
 export const removeNonNumeric = (num) => num.toString().replace(/[^0-9]/g, '');
 
 // option 데이터 리스트를 만들어서 반환하는 함수
-export function optionDataList(sizeArray, colorArray) {
+export function optionDataList(firstArray, secondArray, priceRef) {
   let dataList = [];
   let dataRow = {};
+  let sizeArray = [];
+  let colorArray = [];
   let cnt = 0;
 
-  for (let i = 0; i < sizeArray.length; i++) {
-    dataRow = new Object();
-    for (let j = 0; j < colorArray.length; j++) {
+  // 상품가격을 받아와서 저장
+  const priceValue = priceRef.current.value;
+
+  try {
+    // firstArray, secondArray 둘중 누가 색상값을 가지고 있는지 확인
+    firstArray[0].color
+      ? ((colorArray = [...firstArray]), (sizeArray = [...secondArray]))
+      : ((colorArray = [...secondArray]), (sizeArray = [...firstArray]));
+
+    for (let i = 0; i < sizeArray.length; i++) {
       dataRow = new Object();
-      dataRow.size = sizeArray[i].value;
-      dataRow.color = colorArray[j].color;
-      dataRow.colorName = colorArray[j].value;
-      dataRow.price = 0;
-      dataRow.stockNow = 0;
-      dataRow.stockAdd = 0;
-      dataRow.status = '';
-      dataList[cnt] = dataRow;
-      cnt++; //카운트 1 증가
+      for (let j = 0; j < colorArray.length; j++) {
+        dataRow = new Object();
+        dataRow.size = sizeArray[i].value;
+        dataRow.color = colorArray[j].color;
+        dataRow.colorName = colorArray[j].value;
+        dataRow.price = priceValue;
+        dataRow.stockNow = 0;
+        dataRow.stockAdd = 0;
+        dataRow.status = '';
+        dataList[cnt] = dataRow;
+        cnt++; //카운트 1 증가
+      }
     }
+  } catch (e) {
+    console.log(e);
   }
 
   return dataList;
@@ -36,11 +50,13 @@ export function inputNumberFormat(obj) {
   obj.current.value = comma(uncomma(obj.current.value));
 }
 
+// 콤마 씌우기
 export function comma(str) {
   str = String(str);
   return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
 }
 
+// 콤마 지우기
 export function uncomma(str) {
   str = String(str);
   return str.replace(/[^\d]+/g, '');
